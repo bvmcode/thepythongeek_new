626a5157d2b0524dc05eec566346e6068f873dd5
bvmcode@gmail.com <bvmcode@thepythongeek.com>
2022-12-21 21:37:39 -0500

    tests

--- client/app/tests/home/test_views.py
+++
@@ -1,3 +0,0 @@
-def test_home_view(client):
-    response = client.get("/")
-    assert response.status_code == 200
---
+++ client/app/tests/test_views.py
@@ -0,0 +1,19 @@
+def test_home_view(client):
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_resume_view(client):
+    response = client.get("/resume/")
+    assert response.status_code == 200
+
+
+def test_project_view(client):
+    response = client.get("/project/")
+    assert response.status_code == 200
+
+
+def test_404_view(client):
+    response = client.get("/blah/")
+    assert response.status_code == 404
+    
