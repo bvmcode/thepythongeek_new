8dfb13470a04576d62343f38ac17e2d7155917c0
Barry Martin <bmartin@zelis.com>
2025-03-19 14:58:05 -0400

    update

--- client/app/__init__.py
+++ client/app/__init__.py
@@ -1,12 +1,12 @@
 import os
 
 from flask import Flask, render_template
-from flask_caching import Cache
+# from flask_caching import Cache
 from flask_cors import CORS
 from redis import Redis
 
 redis = Redis(host="redis", port=6379)
-cache = Cache()
+# cache = Cache()
 
 
 def create_app():
@@ -15,7 +15,7 @@ def create_app():
     app_settings = os.getenv("APP_SETTINGS")
     app.config.from_object(app_settings)
     CORS(app)
-    cache.init_app(app)
+    # cache.init_app(app)
 
     from app.home.views import home_bp
     from app.project.views import project_bp
--- client/app/project/views.py
+++ client/app/project/views.py
@@ -6,7 +6,7 @@ import pytz
 import time
 
 import boto3
-from app import cache
+# from app import cache
 from botocore.exceptions import ClientError
 from flask import Blueprint, render_template, make_response, jsonify
 from flask_restx import Api, Resource
@@ -20,44 +20,44 @@ project_bp = Blueprint("project", __name__, url_prefix="/project")
 api = Api(project_bp)
 
 
-def get_data(date):
-    s3 = boto3.resource("s3")
-    try:
-        content_object = s3.Object("artist-followers", f"{date}.json")
-        file_content = content_object.get()["Body"].read().decode("utf-8")
-        return json.loads(file_content)
-    except ClientError:
-        return None
+# def get_data(date):
+#     s3 = boto3.resource("s3")
+#     try:
+#         content_object = s3.Object("artist-followers", f"{date}.json")
+#         file_content = content_object.get()["Body"].read().decode("utf-8")
+#         return json.loads(file_content)
+#     except ClientError:
+#         return None
 
 
-@project_bp.route("/")
-def project():
-    return render_template(
-        "project.html", title="\\\\sample_project\\\\", title_img="project.png"
-    )
+# @project_bp.route("/")
+# def project():
+#     return render_template(
+#         "project.html", title="\\\\sample_project\\\\", title_img="project.png"
+#     )
 
-@project_bp.route("/music")
-def project2():
-    return render_template(
-        "project2.html", title="\\\\sample_project\\\\", title_img="project2.jpg"
-    )
+# @project_bp.route("/music")
+# def project2():
+#     return render_template(
+#         "project2.html", title="\\\\sample_project\\\\", title_img="project2.jpg"
+#     )
 
 
-@api.route("/api/artists/<artist>/weekly")
-class ArtistApi(Resource):
-    @cache.cached(timeout=1000, query_string=True)
-    def get(self, artist):
-        # hard code due to twitter API now chargin
-        start = datetime(2023, 1, 1)
-        end = datetime(2023, 1, 7)
-        data = {}
-        while start <= end:
-            date = start.strftime("%Y%m%d")
-            json_data = get_data(date)
-            if json_data:
-                data[date] = json_data[artist]
-            start = start + timedelta(days=1)
-        return data
+# @api.route("/api/artists/<artist>/weekly")
+# class ArtistApi(Resource):
+#     @cache.cached(timeout=1000, query_string=True)
+#     def get(self, artist):
+#         # hard code due to twitter API now chargin
+#         start = datetime(2023, 1, 1)
+#         end = datetime(2023, 1, 7)
+#         data = {}
+#         while start <= end:
+#             date = start.strftime("%Y%m%d")
+#             json_data = get_data(date)
+#             if json_data:
+#                 data[date] = json_data[artist]
+#             start = start + timedelta(days=1)
+#         return data
 
     
 class Database:
@@ -205,7 +205,7 @@ def date_range():
 
 @api.route("/api/predictions")
 class PredictionApi(Resource):
-    @cache.cached(timeout=1000, query_string=True)
+    # @cache.cached(timeout=1000, query_string=True)
     def get(self):
         dates = date_range()
         predictions = []
